ef4a7ba4a9fcaebeed16f9012d3acadcd318cea0
Pingoin <pdrente@googlemail.com>
2022-10-27 20:37:12 +0200

    new data structure via maps

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,18 @@
-use std::fs::File;
-use std::io::prelude::*;
-mod recipie;
-use crate::recipie::{Recipie, Ressource, Target};
-use serde_derive::{Deserialize, Serialize};
+use std::collections::HashMap;
 
-#[derive(Deserialize, Serialize, Debug)]
-struct InputFile {
-    #[serde(default)]
-    resources: Vec<Ressource>,
-    #[serde(default)]
-    recepies: Vec<Recipie>,
-    #[serde(default)]
-    targets: Vec<Target>,
-}
+use prod_lines::recipie::{read_from_file, Recipie};
 
 fn main() -> std::io::Result<()> {
     let path = String::from("production.json");
-    let mut input_file = match File::open(&path) {
-        Ok(file) => file,
-        Err(error) => panic!("Problem opening the file: {:?}", error),
-    };
+    let mut data =read_from_file(&path);
+    let mut result:HashMap<String,Recipie>=HashMap::new();
 
-    let mut string_data = String::from("");
-    let bla = input_file.read_to_string(&mut string_data);
-    match bla {
-        Ok(bldup) => println!("size:{}", bldup),
-        Err(error) => panic!("Problem opening the file: {:?}", error),
-    };
-    
+    data.productions.iter_mut().for_each(|(_,prod)|{
+        let index =prod.recipies[0].clone();
+        let recipie = data.recepies[&index].clone();
 
-    let data = {
-        match serde_json::from_str::<InputFile>(&string_data) {
-            Ok(result) => result,
-            Err(error) => panic!("Problem : {:?}", error),
-        }
-    };
-
-    let json_data = match serde_json::to_string_pretty(&data) {
-        Ok(res) => res,
-        Err(_) => String::from("Müll"),
-    };
-    let mut input_file = match File::create(&path) {
-        Ok(file) => file,
-        Err(error) => panic!("Problem opening the file for writing: {:?}", error),
-    };
-    match input_file.write_all(&json_data.as_bytes()){
-        Ok(_)=>{},
-        Err(e)=>panic!("Problem writing file: {}",e),
-    };
-    Ok(())
+        result.insert(index, recipie);
+    });
+    dbg!(result);
+    data.write_to_file(&path)
 }
--- src/recipie.rs
+++ src/recipie.rs
@@ -1,10 +1,10 @@
-
 use serde_derive::{Deserialize, Serialize};
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::{prelude::*, Error};
 
-#[derive(Deserialize, Serialize, Debug)]
+#[derive(Deserialize, Serialize, Debug,Clone)]
 pub struct Recipie {
-    #[serde(default)]
-    pub id: u32,
     #[serde(default)]
     pub name: String,
     #[serde(default)]
@@ -13,42 +13,104 @@ pub struct Recipie {
     pub outputs: Vec<RessourceUsed>,
     #[serde(default)]
     pub duration: f32,
-    #[serde(default)]
-    pub modifier: f32,
 }
 
-#[derive(Deserialize, Serialize, Debug)]
+#[derive(Deserialize, Serialize, Debug,Clone)]
 
 pub struct Production {
     #[serde(default)]
     pub name: String,
-    #[serde(default)]
+    #[serde(default = "float1")]
     pub factor: f32,
+    #[serde(default)]
+    pub recipies: Vec<String>,
+    #[serde(default = "default_true")]
+    #[serde(rename = "devideRecipies")]
+    pub devide_recipies: bool,
 }
 
-#[derive(Deserialize, Serialize, Debug)]
+fn float1() -> f32 {
+    1.0
+}
+
+fn default_true() -> bool {
+    true
+}
+
+#[derive(Deserialize, Serialize, Debug,Clone)]
 pub struct Ressource {
-    #[serde(default)]
-    pub id: u32,
     #[serde(default)]
     pub name: String,
     #[serde(default)]
     pub value: f32,
 }
-#[derive(Deserialize, Serialize, Debug)]
+
+#[derive(Deserialize, Serialize, Debug,Clone)]
 pub struct RessourceUsed {
     #[serde(default)]
-    pub ressource: u32,
+    pub ressource: String,
     #[serde(default)]
     pub amount: f32,
 }
 
-#[derive(Deserialize, Serialize, Debug)]
+#[derive(Deserialize, Serialize, Debug,Clone)]
 pub struct Target {
     #[serde(default)]
-    pub ressource: u32,
+    pub ressource: String,
     #[serde(default)]
     pub input: bool,
     #[serde(default)]
     pub amount: f32,
+}
+
+#[derive(Deserialize, Serialize, Debug,Clone)]
+pub struct InputFile {
+    #[serde(default)]
+    pub resources: HashMap<String,Ressource>,
+    #[serde(default)]
+    pub recepies: HashMap<String,Recipie>,
+    #[serde(default)]
+    pub targets: HashMap<String,Target>,
+    #[serde(default)]
+    pub productions: HashMap<String,Production>,
+}
+
+pub fn read_from_file(path: &String) -> InputFile {
+    let mut input_file = match File::open(path) {
+        Ok(file) => file,
+        Err(error) => panic!("Problem opening the file: {:?}", error),
+    };
+
+    let mut string_data = String::from("");
+    let bla = input_file.read_to_string(&mut string_data);
+    match bla {
+        Ok(_) => {},
+        Err(error) => panic!("Problem opening the file: {:?}", error),
+    };
+
+    let data = {
+        match serde_json::from_str::<InputFile>(&string_data) {
+            Ok(result) => result,
+            Err(error) => panic!("Problem : {:?}", error),
+        }
+    };
+    data
+}
+
+impl InputFile {
+    pub fn write_to_file(&self, path: &String) -> Result<(), Error> {
+        let json_data = match serde_json::to_string_pretty(self) {
+            Ok(res) => res,
+            Err(error) => return Err(Error::from(error)),
+        };
+        let mut input_file = match File::create(&path) {
+            Ok(file) => file,
+            Err(error) => return Err(error),
+        };
+        match input_file.write_all(&json_data.as_bytes()) {
+            Ok(_) => {}
+            Err(error) => return Err(error),
+        };
+        Ok(())
+    }
 }
