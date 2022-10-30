1b45e29b1d924b136b3fc5cff54a4532d0c032d8
Pingoin <pdrente@googlemail.com>
2022-10-30 19:29:08 +0100

    production line build

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,50 @@
 use std::collections::HashMap;
 
-use prod_lines::{recipie::Recipie,read_from_file};
+use prod_lines::{
+    read_from_file,
+    recipie::{IOtype, ProductionStep},
+};
 
 fn main() -> std::io::Result<()> {
     let path = String::from("production.json");
-    let mut data =read_from_file(&path);
-    let mut result:HashMap<String,Recipie>=HashMap::new();
+    let data = read_from_file(&path);
 
-    data.productions.iter_mut().for_each(|(_,prod)|{
-        let index =prod.recipies[0].clone();
-        let recipie = data.recepies[&index].clone();
+    let mut wighted_recipies: HashMap<String, ProductionStep> = HashMap::new();
+    for (key, val) in data.recepies.iter() {
+        wighted_recipies.insert(key.clone(), val.to_production_step());
+    }
 
-        result.insert(index, recipie);
-    });
-    dbg!(result);
+    for (_, production) in data.productions.iter() {
+        for recipie_id in production.recipies.iter() {
+            wighted_recipies.entry(recipie_id.clone()).and_modify(|f| {
+                f.production_capacity += production.factor / f.duration;
+            });
+        }
+    }
+    let mut production_line: Vec<ProductionStep> = Vec::new();
+    {
+        let mut produced_ressources: Vec<String> = Vec::new();
+        let mut already_produced_ressources: Vec<String> = Vec::new();
+        let mut inserted_keys: Vec<String> = Vec::new();
+
+        for (_, recipie) in wighted_recipies.iter() {
+            recipie.add_output_to_list(&mut produced_ressources);
+        }
+
+        while production_line.len() < wighted_recipies.len() {
+            for (key, recipie) in wighted_recipies.iter() {
+                if (!recipie.list_contains(IOtype::Input, &produced_ressources)
+                    || recipie.list_contains(IOtype::Input, &already_produced_ressources))
+                    && !inserted_keys.contains(key)
+                {
+                    production_line.push(recipie.clone());
+                    inserted_keys.push(key.clone());
+                    recipie.add_output_to_list(&mut already_produced_ressources);
+                }
+            }
+        }
+    }
+    
+    
     data.write_to_file(&path)
 }
--- src/recipie.rs
+++ src/recipie.rs
@@ -1,7 +1,7 @@
-use serde_derive::{Deserialize, Serialize};
 use super::ressource::RessourceUsed;
+use serde_derive::{Deserialize, Serialize};
 
-#[derive(Deserialize, Serialize, Debug,Clone)]
+#[derive(Deserialize, Serialize, Debug, Clone)]
 pub struct Recipie {
     #[serde(default)]
     pub name: String,
@@ -11,4 +11,59 @@ pub struct Recipie {
     pub outputs: Vec<RessourceUsed>,
     #[serde(default)]
     pub duration: f32,
+}
+
+impl Recipie {
+
+    pub fn to_production_step(&self) -> ProductionStep {
+        ProductionStep {
+            inputs: self.inputs.clone(),
+            outputs: self.outputs.clone(),
+            production_capacity: 0.0,
+            duration:self.duration,
+        }
+    }
+
+}
+
+pub enum IOtype {
+    Input,
+    Output,
+}
+
+#[derive(Deserialize, Serialize, Debug, Clone)]
+pub struct ProductionStep {
+    pub inputs: Vec<RessourceUsed>,
+    pub outputs: Vec<RessourceUsed>,
+    pub duration:f32,
+    pub production_capacity: f32,
+}
+
+impl ProductionStep {
+    pub fn add_output_to_list(&self, list: &mut Vec<String>) {
+        self.outputs.iter().for_each(|prod| {
+            let production = prod.ressource.clone();
+            if !list.contains(&production) {
+                list.push(production)
+            }
+        });
+    }
+
+    pub fn list_contains(&self, input_output: IOtype, list: &Vec<String>) -> bool {
+        let mut result = false;
+        let put_list;
+
+        match input_output {
+            IOtype::Input => put_list = &self.inputs,
+            IOtype::Output => put_list = &self.outputs,
+        }
+
+        put_list.iter().for_each(|prod| {
+            if list.contains(&prod.ressource) {
+                result = true;
+            }
+        });
+
+        result
+    }
 }
